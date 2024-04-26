4f6b02678f9b1d5383f81a0c123d4c04805cc6db
Traxim User <traximuser20@gmail.com>
2024-04-27 00:49:30 +0500

    --initial-commit

---
+++ android/app/src/main/java/com/calendar/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.calendar.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ src/App.vue
@@ -0,0 +1,70 @@
+<script>
+import FullCalendar from "@fullcalendar/vue3";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import listPlugin from "@fullcalendar/list";
+import interactionPlugin from "@fullcalendar/interaction";
+
+export default {
+  components: {
+    FullCalendar, // make the <FullCalendar> tag available
+  },
+  data() {
+    return {
+      calendarOptions: {
+        plugins: [dayGridPlugin, interactionPlugin, timeGridPlugin, listPlugin],
+        defaultView: "dayGridMonth",
+        initialView: "dayGridMonth",
+        dateClick: this.handleDateClick,
+        headerToolbar: {
+          center: "title",
+          left: "prev,next",
+          right: "dayGridMonth,timeGridWeek,timeGridDay,listWeek",
+        },
+        weekend: false,
+        events: "https://fullcalendar.io/api/demo-feeds/events.json",
+        contentHeight: 760,
+        aspectRatio: 0.5,
+        
+      },
+    };
+  },
+  methods: {
+    handleDateClick: function (arg) {
+      alert("date click! " + arg.dateStr);
+    },
+  },
+};
+</script>
+<template>
+  <div class="w-full h-full px-5 mt-4">
+    <FullCalendar :options="calendarOptions" />
+  </div>
+</template>
+
+<style>
+.fc .fc-toolbar.fc-header-toolbar {
+  display: block;
+}
+
+.fc-button {
+  border-radius: 5px;
+  font-size: 14px;
+  padding: 5px 10px;
+  justify-items: center;
+  /* display: flex */
+}
+
+.fc .fc-toolbar-title {
+    font-size: 1.75em;
+    margin: 0px;
+    text-align: center;
+}
+
+.fc .fc-button-group {
+    display: inline-flex;
+    position: relative;
+    vertical-align: middle;
+    display: flex;
+}
+</style>
